197237d3cfdb3819dbff0235d774f910a050e969
Min Hanahaki <amankeldiamina3@gmail.com>
2024-10-30 16:40:55 +0500

    fix alt

--- scripts/index.js
+++ scripts/index.js
@@ -4,36 +4,38 @@ const template = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 
+const list = document.querySelector(".places__list");
+
 // @todo: Функция создания карточки
 
 function createCard(card, callback) {
-  const cardNode = template.querySelector(".card").cloneNode(true);
+    const cardNode = template.querySelector(".card").cloneNode(true);
 
-  const image = cardNode.querySelector(".card__image");
-  image.src = card.link;
+    const image = cardNode.querySelector(".card__image");
+    image.src = card.link;
+    image.alt = card.name;
 
-  const titleNode = cardNode.querySelector(".card__title");
-  titleNode.textContent = card.name;
+    const titleNode = cardNode.querySelector(".card__title");
+    titleNode.textContent = card.name;
 
-  const deleteButton = cardNode.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', function () {
-    callback(cardNode);
-  });
+    const deleteButton = cardNode.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function () {
+        callback(cardNode);
+    });
 
-  return cardNode;
+    return cardNode;
 }
 
 // @todo: Функция удаления карточки
 
 function deleteCard(cardNode) {
     cardNode.remove();
-};
+}
 
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach(function (element) {
-  const card = createCard(element, deleteCard);
+    const card = createCard(element, deleteCard);
 
-  const list = document.querySelector(".places__list");
-  list.append(card);
+    list.append(card);
 });
