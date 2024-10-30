2e9232e3c9a5d57faab980014cef658338af5e0f
Min Hanahaki <amankeldiamina3@gmail.com>
2024-10-30 16:16:19 +0500

    add cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
 
+const template = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 
+function createCard(card, callback) {
+  const cardNode = template.querySelector(".card").cloneNode(true);
+
+  const image = cardNode.querySelector(".card__image");
+  image.src = card.link;
+
+  const titleNode = cardNode.querySelector(".card__title");
+  titleNode.textContent = card.name;
+
+  const deleteButton = cardNode.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function () {
+    callback(cardNode);
+  });
+
+  return cardNode;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(cardNode) {
+    cardNode.remove();
+};
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (element) {
+  const card = createCard(element, deleteCard);
+
+  const list = document.querySelector(".places__list");
+  list.append(card);
+});
